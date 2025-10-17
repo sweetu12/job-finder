afa635acc9db2dc26287b29d0f69568e05962e94
Sitanshu D <34069089+sweetu12@users.noreply.github.com>
2025-10-16 23:26:25 -0400

    Change posted_within parameter from '1h' to '12h'

--- jobfinder.py
+++ jobfinder.py
@@ -49,7 +49,7 @@ def fetch_jobright():
         "country": "US",
         "job_type": ",".join(job_types),
         "experience_level": ",".join(experience_levels),
-        "posted_within": "1h"
+        "posted_within": "12h"
     }
 
     try:
